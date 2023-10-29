bdf1329ad0e67a8c83bb37bb940f3f3a914bf2f7
leen-gh <leen.ghaleb.d@gmail.com>
2023-10-30 00:44:21 +0300

    assigment is  done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 =31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = parseInt((Math.random() *20) +1)
+let num3 = 20
+let num4 = 5
+let mod =  num3/num4
+
+let max = Math.max(15, 10 ,20 ,16)
+
+
+
+
